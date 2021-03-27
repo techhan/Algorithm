e739418995fae19215b0a5053dc8f4b3e1c05350
techhan <it_hs@naver.com>
2021-03-27 21:58:37 +0900

    doit chap 01-12~ 01-17 upload

---
+++ Doit/chap01/Q14.java
@@ -0,0 +1,21 @@
+package chap01;
+
+import java.util.Scanner;
+
+public class Q14 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("사각형을 출력합니다.");
+		System.out.print("단 수 : ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
